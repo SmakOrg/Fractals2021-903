8be6350beb562e97f08493d77277aad8e57b132d
Sergey Makletsov <smaklets@kpfu.ru>
2021-11-19 20:22:42 +0300

    Распараллеливание построения фрайктала на пуле потоков - написано после пар

--- src/main/kotlin/ru/smak/ui/painting/fractals/FractalPainter.kt
+++ src/main/kotlin/ru/smak/ui/painting/fractals/FractalPainter.kt
@@ -8,7 +8,10 @@ import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics
 import java.awt.image.BufferedImage
+import java.util.concurrent.Callable
 import java.util.concurrent.Executors
+import java.util.concurrent.Future
+import java.util.concurrent.LinkedBlockingQueue
 
 class FractalPainter(
     val fractal: AlgebraicFractal,
@@ -22,30 +25,56 @@ class FractalPainter(
             plane.pixelSize = value
         }
 
+    private val threadCount = 16
+    private var fracService = Executors.newFixedThreadPool(threadCount)
+
     override fun paint(g: Graphics) {
-        val threadCount = 16
-        val fracService = Executors.newFixedThreadPool(threadCount)
+        val t1 = System.currentTimeMillis()
         with(plane){
-            for (i in 0 until width){
-                fracService.submit {
-                    val img = BufferedImage(1, height, BufferedImage.TYPE_INT_RGB)
-                    for (j in 0..height) {
-                        with (img.graphics)
-                        {
-                            color = colorFunction(fractal.isInSet(
-                                complex(
-                                    xScr2Crt(i),
-                                    yScr2Crt(j)
-                                )
-                            ))
-                            fillRect(0, j, 1, 1)
-                        }
+            if (width <= 0 || height <= 0) return
+            val q = LinkedBlockingQueue<Pair<Int, Future<BufferedImage>>>()
+            if (!(fracService.isShutdown || fracService.isTerminated)) {
+                fracService.shutdown()
+                fracService = Executors.newFixedThreadPool(threadCount)
+            }
+            val subWidth = 10
+            for (i in 0 until (width / subWidth)){
+                q.put(
+                    Pair(
+                        i * subWidth,
+                        fracService.submit(Callable {
+                            val wsz = subWidth + if (i+1 == width/subWidth) width % subWidth else 0
+                            val img = BufferedImage(wsz, height, BufferedImage.TYPE_INT_RGB)
+                            for (k in 0 until wsz) {
+                                for (j in 0..height) {
+                                    with (img.graphics) {
+                                        color = colorFunction(
+                                            fractal.isInSet(
+                                                complex(
+                                                    xScr2Crt(i*wsz+k),
+                                                    yScr2Crt(j)
+                                                )
+                                            )
+                                        )
+                                        fillRect(k, j, 1, 1)
+                                    }
+                                }
+                            }
+                            img
+                        })
+                    )
+                )
+            }
+            try {
+                for (i in 0 until (width / subWidth)) {
+                    q.take().run {
+                        g.drawImage(second.get(), first, 0, null)
                     }
-                    //synchronized(g){
-                    g.drawImage(img, i, 0, null)
-                    //}
                 }
+            } catch (ex: InterruptedException){
             }
         }
+        val t2 = System.currentTimeMillis()
+        println(t2 - t1)
     }
 }
