1036ec8995a433b3b5cb6f0451bc56a745b62faa
Sergey Makletsov <smaklets@kpfu.ru>
2021-11-19 18:50:16 +0300

    Распараллеливание построения фрайктала на а отдельных потоках - закончено на паре

--- src/main/kotlin/ru/smak/ui/painting/fractals/FractalPainter.kt
+++ src/main/kotlin/ru/smak/ui/painting/fractals/FractalPainter.kt
@@ -8,7 +8,7 @@ import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics
 import java.awt.image.BufferedImage
-import kotlin.concurrent.thread
+import java.util.concurrent.Executors
 
 class FractalPainter(
     val fractal: AlgebraicFractal,
@@ -24,33 +24,27 @@ class FractalPainter(
 
     override fun paint(g: Graphics) {
         val threadCount = 16
+        val fracService = Executors.newFixedThreadPool(threadCount)
         with(plane){
-            List<Thread>(threadCount) { tNum ->
-                thread {
-                    val w = width / threadCount
-                    val start = tNum * w
-                    val end   = (tNum + 1) * w - 1 + if (tNum == threadCount-1) width % threadCount else 0
-                    val img = BufferedImage(end-start+1, height, BufferedImage.TYPE_INT_RGB)
-                    for (i in start..end) {
-                        for (j in 0..height) {
-                            with (img.graphics)
-                            {
-                                color = colorFunction(fractal.isInSet(
-                                    complex(
-                                        xScr2Crt(i),
-                                        yScr2Crt(j)
-                                    )
-                                ))
-                                fillRect(i - start, j, 1, 1)
-                            }
+            for (i in 0 until width){
+                fracService.submit {
+                    val img = BufferedImage(1, height, BufferedImage.TYPE_INT_RGB)
+                    for (j in 0..height) {
+                        with (img.graphics)
+                        {
+                            color = colorFunction(fractal.isInSet(
+                                complex(
+                                    xScr2Crt(i),
+                                    yScr2Crt(j)
+                                )
+                            ))
+                            fillRect(0, j, 1, 1)
                         }
                     }
-                    synchronized(g){
-                        g.drawImage(img, start, 0, null)
-                    }
+                    //synchronized(g){
+                    g.drawImage(img, i, 0, null)
+                    //}
                 }
-            }.forEach {
-                it.join()
             }
         }
     }
